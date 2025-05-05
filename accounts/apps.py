2cb9520ae9f32afdad4631a2dfa68c339f6ab1fd
Eliezer <alencareliezer20@gmail.com>
2025-05-05 00:07:08 -0300

    aaaaaaaaaaaaaaa

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,8 +1,10 @@
+# accounts/apps.py
 import os
 from django.apps import AppConfig
 from django.db.utils import OperationalError, ProgrammingError
 
 class AccountsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
 
     def ready(self):
@@ -10,15 +12,13 @@ class AccountsConfig(AppConfig):
         from django.conf import settings
 
         User = get_user_model()
-        cpf      = settings.ADMIN_CPF
-        password = settings.ADMIN_PASSWORD
+        cpf      = getattr(settings, 'ADMIN_CPF', None)
+        password = getattr(settings, 'ADMIN_PASSWORD', None)
+        email    = getattr(settings, 'ADMIN_EMAIL', None)
 
-        # só tenta criar se ambas variáveis existirem
         if cpf and password:
             try:
                 if not User.objects.filter(cpf=cpf).exists():
                     User.objects.create_superuser(cpf=cpf, password=password, email=email or '')
             except (OperationalError, ProgrammingError):
-                # o banco ainda não está pronto (ou rodando migrate não foi chamado)
-                # ou outra migração pendente: ignore esse erro
                 pass
